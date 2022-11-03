b99b14bf9b1ac2ebda4ae08186938022a7e721de
Anssi Pitkänen <anssi.pitkan@gmail.com>
2022-11-03 16:47:21 +0200

    simpleProducer needs not extend FibonacciBase

--- src/main/scala/app/kafka/SimpleKafkaProducer.scala
+++ src/main/scala/app/kafka/SimpleKafkaProducer.scala
@@ -1,10 +1,9 @@
 package app.kafka
 
-import app.streams.FibonacciStreamBase
 import cats.effect.Async
 import fs2.kafka.{KafkaProducer, ProducerRecord, ProducerRecords, ProducerSettings}
 
-class SimpleKafkaProducer[F[_] : Async](producerSettingsMain: ProducerSettings[F, Option[String], Array[Byte]]) extends FibonacciStreamBase {
+class SimpleKafkaProducer[F[_] : Async](producerSettingsMain: ProducerSettings[F, Option[String], Array[Byte]]) {
   import cats.implicits._
 
   def send(record: ProducerRecord[Option[String], Array[Byte]]): F[Either[Throwable, Unit]] = {
