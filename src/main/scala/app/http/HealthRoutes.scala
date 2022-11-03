087b8742a225de18889d54c4c45f377ce6aa48e0
Anssi Pitkänen <anssi.pitkan@gmail.com>
2022-11-03 16:53:32 +0200

    add readiness endpoint

--- src/main/scala/app/http/HealthRoutes.scala
+++ src/main/scala/app/http/HealthRoutes.scala
@@ -1,10 +1,11 @@
 package app.http
 
 import cats.Monad
+import com.redis.RedisClient
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
-final class HealthRoutes[F[_] : Monad]() {
+final class HealthRoutes[F[_] : Monad](redis: RedisClient) {
 
   private val dsl = Http4sDsl[F]
   import dsl._
@@ -12,6 +13,10 @@ final class HealthRoutes[F[_] : Monad]() {
   def routes: HttpRoutes[F] =
     HttpRoutes.of[F] {
       case GET -> Root / "liveness" =>
+        Ok()
+
+      case GET -> Root / "readiness" =>
+        redis.get("dummy")
         Ok()
     }
 }
