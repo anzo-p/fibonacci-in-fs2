86123dce5c22a4d4f25ba0af82716fb2fff5dd98
Anssi Pitkänen <anssi.pitkan@gmail.com>
2022-11-04 20:22:02 +0200

    hide defaule apply for DLQMessage

--- src/main/scala/app/kafka/DLQMessage.scala
+++ src/main/scala/app/kafka/DLQMessage.scala
@@ -6,11 +6,25 @@ case class DLQMessage(eventKey: String, error: Json, stackTrace: Json)
 
 object DLQMessage {
   import io.circe._
-  import io.circe.generic.semiauto._
   import io.circe.syntax._
 
-  implicit val decode: Decoder[DLQMessage] = deriveDecoder
-  implicit val encode: Encoder[DLQMessage] = deriveEncoder
+  implicit val encoder: Encoder[DLQMessage] = (message: DLQMessage) =>
+    Json.obj(
+      ("eventKey", Json.fromString(message.eventKey)),
+      ("error", message.error),
+      ("stackTrace", message.stackTrace)
+    )
+
+  implicit val decoder: Decoder[DLQMessage] = (c: HCursor) =>
+    for {
+      key   <- c.downField("eventKey").as[String]
+      error <- c.downField("error").as[Json]
+      trace <- c.downField("stackTrace").as[Json]
+    } yield {
+      new DLQMessage(key, error, trace)
+    }
+
+  private def apply = new DLQMessage(_, _, _)
 
   def apply(eventKey: String, throwable: Throwable): DLQMessage =
     DLQMessage(
